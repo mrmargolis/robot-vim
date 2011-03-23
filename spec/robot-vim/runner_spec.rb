1bdedc0f4c35b3f9a882bd43e80e7f079318800c
Matt Margolis <matt@mattmargolis.net>
2011-03-22 20:46:56 -0500

    Remove rspec context that wasn't justifying its existence

--- spec/robot-vim/runner_spec.rb
+++ spec/robot-vim/runner_spec.rb
@@ -35,41 +35,38 @@ describe RobotVim::Runner do
     let(:vimrc_path){"/testing/vimrc"}
     let(:robot){RobotVim::Runner.new(:vim => vim_path, :vimrc => vimrc_path)}
 
-    context "Running commands" do
-      let(:commands){"some vim commands"}
-      let(:input_file){"some/path/to/a/file"}
+    let(:commands){"some vim commands"}
+    let(:input_file){"some/path/to/a/file"}
 
-      def run_robot
-        robot.run(:commands => commands, :input_file => input_file)
-      end
+    def run_robot
+      robot.run(:commands => commands, :input_file => input_file)
+    end
 
-      it "invokes the correct vim" do
-        Kernel.should_receive(:`).with(/#{vim_path}/)
+    it "invokes the correct vim" do
+      Kernel.should_receive(:`).with(/#{vim_path}/)
         run_robot
-      end
+    end
 
-      it "uses the correct vimrc" do
-        Kernel.should_receive(:`).with(/-u #{vimrc_path}/)
+    it "uses the correct vimrc" do
+      Kernel.should_receive(:`).with(/-u #{vimrc_path}/)
+      run_robot
+    end
+
+    it "runs against the requested input file" do
+      Kernel.should_receive(:`).with(/#{input_file}$/)
         run_robot
-      end
+    end
 
-      it "runs against the requested input file" do
-        Kernel.should_receive(:`).with(/#{input_file}$/)
-        run_robot
-      end
+    it "runs vim without swap files so vim doesn't show warnings" do
+      Kernel.should_receive(:`).with(/-n/)
+      run_robot
+    end
 
-      it "runs vim without swap files so vim doesn't show warnings" do
-        Kernel.should_receive(:`).with(/-n/)
-        run_robot
-      end
-
-      it "invokes vim with a script file" do
-        script_file_path = "path/to/script/file"
-        RobotVim::ScriptFile.stub(:open).and_yield(script_file_path)
-        Kernel.should_receive(:`).with(/-s #{script_file_path}/)
-        run_robot
-      end
-
+    it "invokes vim with a script file" do
+      script_file_path = "path/to/script/file"
+      RobotVim::ScriptFile.stub(:open).and_yield(script_file_path)
+      Kernel.should_receive(:`).with(/-s #{script_file_path}/)
+      run_robot
     end
 
   end
