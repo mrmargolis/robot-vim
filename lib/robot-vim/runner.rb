2680c8dd61318dc8c3c642a4c8ed4b136243ff0f
Matt Margolis <matt@mattmargolis.net>
2011-03-27 14:31:49 -0500

    Move write and vim closing command strings out of Runner#run.

--- lib/robot-vim/runner.rb
+++ lib/robot-vim/runner.rb
@@ -11,9 +11,8 @@ module RobotVim
     end
 
     def run(args={})
-      output_file_name = FileNameGenerator.generate
-      commands = args[:commands] + ":w #{output_file_name}"
-      commands = commands + "\n:%bd!\n:q!\n"
+      output_file_name = RobotVim::FileNameGenerator.generate
+      commands = args[:commands] + output_write_command(output_file_name) + vim_close_commands
       invoke_vim(commands, args[:input_file])
       return File.read(output_file_name)
     ensure
@@ -26,6 +25,14 @@ module RobotVim
       ScriptFile.open(commands) do |script_file_path|
         Kernel.send(:`, "#{self.vim_binary} -n -s #{script_file_path} #{input_file}")
       end
+    end
+
+    def output_write_command(output_file_name)
+      ":w #{output_file_name}"
+    end
+
+    def vim_close_commands
+      "\n:%bd!\n:q!\n"
     end
   end
 end
